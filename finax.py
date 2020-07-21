d366f820af77722df49517c2c95dd0dd9aca8c18
yason <michal.pieprzycki@fx-tronik.pl>
2020-07-21 10:17:22 +0200

    replace data in clipboard directly

--- finax.py
+++ finax.py
@@ -1,7 +1,10 @@
 import datetime
+import clipboard
+from tkinter import Tk
 
-input_data = """
-"""
+root = Tk()
+root.withdraw()
+input_data = root.clipboard_get()
 
 mi_list = ['XBLC', 'XXSC', 'XSX6']
 
@@ -9,11 +12,12 @@ data = input_data.split("\n")
 result = ''
 
 for row in data:
+    #row.rstrip()
     values = row.split("\t") #split rows into list of values
+    #print(values)
     new_date = datetime.datetime.strptime(values[0], "%d.%m.%Y").strftime("%Y-%m-%d") #change date format to YYYY-MM-DD
     
     ticker = values[-1]
-    
     ticker = ticker.replace("GR","MI") if ticker.split('.')[0] in mi_list else ticker.replace("GR", "DE") #change ticker to one tracked by myfund
     ticker = 'BORSE_' + ticker #add internal myfund prefix 
     
@@ -23,5 +27,6 @@ for row in data:
     new_list = [new_date, ticker, bos, amount, price, "0", "0"] #create new list
     new_row = ";".join(new_list) + "\n" #format list into single csv row
     result += new_row #add row to results
-    
-print(result)
+
+print(result)    
+clipboard.copy(result)
